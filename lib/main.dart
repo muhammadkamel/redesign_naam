0eac6671b9dbbf12fbe4854c1b7a9004c3ddd60a
muhammadkamel <mu7ammadkamel@gmail.com>
2020-07-21 13:40:50 +0200

    Resdign Naam

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Montserrat'),
       initialRoute: 'Cycles',
       routes: {
