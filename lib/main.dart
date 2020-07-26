78de2ae11a5858137726fd7f0dc8b4937802501a
muhammadkamel <mu7ammadkamel@gmail.com>
2020-07-26 10:51:55 +0200

    'Upadtes'

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:redesign_naam/widgets/alarms/alarms.dart';
 import 'package:redesign_naam/widgets/cycles/cycles.dart';
-
-import 'widgets/custom_painter/draw_painter.dart';
 
 void main() => runApp(MyApp());
 
@@ -34,78 +34,135 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  PageController _pageController;
-  // ignore: unused_field
-  TabController _tabController;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   int currPage = 0;
+  AnimationController controller;
+  Animation<Offset> animation;
 
   @override
   initState() {
     super.initState();
-    print('I have do this!');
-    _tabController = TabController(
-      length: 2,
+    controller = AnimationController(
       vsync: this,
-    );
-    _pageController = PageController(
-      initialPage: currPage,
-      viewportFraction: 0.8,
-      keepPage: false,
-    );
-    _pageController.addListener(() {
-      setState(() {
-        currPage = _pageController.page.toInt();
+      duration: Duration(seconds: 1),
+    )
+      ..forward(from: 0)
+      ..addListener(() {
+        setState(() {});
       });
-    });
+    animation = Tween(
+      begin: Offset(0.0, -2),
+      end: Offset(0.0, 0.5),
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Curves.easeInOut,
+      ),
+    );
   }
 
   @override
   dispose() {
-    _pageController.dispose();
+    controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
         children: <Widget>[
-          // Create a circle
-          Center(
+          ClipPath(
+            clipper: ClipperOne(),
             child: Container(
               alignment: Alignment.center,
-              color: Colors.grey[50],
-              height: 300,
-              width: 300,
-              child: CustomPaint(
-                painter: MyPainter(),
-                child: Center(
-                  child: Text(
-                    'Hi',
-                    style: const TextStyle(
-                      fontSize: 40.0,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.red,
-                    ),
-                  ),
-                ),
+              decoration: BoxDecoration(
+                color: Colors.deepOrangeAccent,
               ),
             ),
           ),
         ],
       ),
-      // body: SecondCarousel(
-      //   pageController: _pageController,
-      //   posts: posts,
-      //   title: 'Title',
-      // ),
     );
   }
+}
+
+class ClipperOne extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final width = size.width;
+    final height = size.height;
+    final path = Path()
+      ..moveTo(0, 0)
+      ..lineTo(0, height)
+      ..lineTo(width, height)
+      ..lineTo(width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+class MyCliper extends CustomClipper<Path> {
+  // @override
+  // Path getClip(Size size) {
+  //   var path = Path();
+  //   final width = size.width;
+  //   final height = size.height;
+  //   path.moveTo(0, height * 0.33);
+  //   path.lineTo(0, height - 50);
+  //   path.quadraticBezierTo(0, height, 50, height);
+
+  //   path.lineTo(width - 50, height);
+  //   path.quadraticBezierTo(width, height, width, height - 50);
+
+  //   path.lineTo(width, 100);
+  //   path.quadraticBezierTo(width, 0, width - 50 * 3, 100);
+
+  //   path.lineTo(50, height * 0.33 + 10);
+  //   path.quadraticBezierTo(0, height * 0.33 + 50, 0, height * 0.28 + 100);
+  //   return path;
+  // }
+  @override
+  Path getClip(Size size) {
+    // var path = Path();
+    final width = size.width;
+    final height = size.height;
+
+    final double radius = 50;
+
+    Path path = Path()
+      ..lineTo(width, 0)
+      ..arcTo(Rect.fromLTRB(0, radius, radius, height - radius), 0.5 * pi,
+          0.5 * pi, false)
+      ..lineTo(width, height - radius)
+      ..arcTo(
+          Rect.fromCircle(
+              center: Offset(width - radius, height - radius), radius: radius),
+          0,
+          0.5 * pi,
+          false)
+      ..lineTo(width - radius, height)
+      ..arcTo(Rect.fromLTRB(0, height - radius, radius, height), 0.5 * pi,
+          0.5 * pi, false)
+      ..lineTo(0, 0)
+      ..arcTo(Rect.fromLTWH(0, 0, 100, 100), pi, 0.5 * pi, false)
+      ..close();
+    // all values
+    final c1 = Offset(width / 2, height / 2); // C1
+    final c2 = Offset(width - 100, 200); // C2
+    final endPoint = Offset(width, height); // End point bottom
+
+    // path.moveTo(width / 2, 0); // Center point top
+    // path.lineTo(0, height); // Starting point left
+
+    // path.cubicTo(c1.dx, c1.dy, c2.dx, c2.dy, endPoint.dx, endPoint.dy);
+
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
--- lib/widgets/custom_painter/draw_painter.dart
+++ lib/widgets/custom_painter/draw_painter.dart
@@ -1,73 +1,58 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
 class MyPainter extends CustomPainter {
+  var path = Path();
   // Line 1
-  final p1 = Offset(0, 320);
-  final p2 = Offset(300, 320);
-  // Line 2
-  final p3 = Offset(0, 200);
-  final p4 = Offset(300, 200);
-
-  final _paint = Paint()
+  final p1 = Offset(0, 200);
+  final p2 = Offset(300, 200);
+  final _paint1 = Paint()
     ..color = Colors.indigo
-    ..strokeWidth = 12
+    ..strokeWidth = 10.0
     ..style = PaintingStyle.stroke;
 
+// Line 2
+  final p3 = Offset(0, 250);
+  final p4 = Offset(300, 250);
   final _paint2 = Paint()
     ..color = Colors.orange
-    ..strokeWidth = 50
+    ..strokeWidth = 50.0
     ..style = PaintingStyle.stroke;
 
   @override
   void paint(Canvas canvas, Size size) {
-    // final width = size.width;
-    // final height = size.height;
-    canvas.drawLine(p1, p2, _paint);
-    // canvas.drawOval(Rect.fromLTRB(0, 0, width, height), _paint);
+    // Indigo line
+    canvas.drawLine(p1, p2, _paint1);
+    // Orange line
     canvas.drawLine(p3, p4, _paint2);
-    // canvas.drawOval(Rect.fromLTRB(0, 0, width, height), _paint2);
 
-    Paint painter = Paint()..color = Colors.blue;
+    final height = size.height;
+    final width = size.width;
+
+// Draw a circle
+    Paint painter = Paint()..color = Colors.blueAccent;
     canvas.drawArc(
       Rect.fromCenter(
-        center: Offset(size.height / 2, size.width / 2),
-        height: size.height,
-        width: size.width,
+        center: Offset(height / 2, width * 0.0001),
+        height: height,
+        width: width,
       ),
-      math.pi,
-      math.pi,
-      false,
+      pi,
+      pi * 2,
+      true,
       painter,
     );
 
-    Paint backCirclePaint = Paint()
-      ..style = PaintingStyle.fill
-      ..color = Colors.blue;
-
-    Paint frontCirclePaint = Paint()
-      ..style = PaintingStyle.fill
-      ..color = Colors.white;
-
-    num percentage;
-    canvas.drawArc(
-        Rect.fromCircle(
-            center: Offset(size.width / 2, size.height / 2), radius: 100.0),
-        0.0,
-        -(2 * math.pi * percentage) / 100,
-        true,
-        backCirclePaint);
-
-// percentage is responsible for the amount(angle) of arc you want to build.
-
-    canvas.drawArc(
-        Rect.fromCircle(
-            center: Offset(size.width / 2 + 10.0, size.height / 2),
-            radius: 90.0),
-        0.0,
-        -(2 * math.pi * percentage) / 100,
-        true,
-        frontCirclePaint);
+    // Draw somethin strange
+    final p7 = Offset(height * -0.1, 0);
+    final p8 = Offset(height * 1.10, 0);
+    final _paint3 = Paint()
+      ..color = Colors.greenAccent
+      ..strokeWidth = 200.0
+      ..style = PaintingStyle.fill;
+    canvas.drawLine(p7, p8, _paint3);
+    path.lineTo(0, height);
+    path.moveTo(height, width);
   }
 
   @override
